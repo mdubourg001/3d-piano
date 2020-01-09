bbe08966b8ae1acaa0477da8eb7a6f35352568d9
NicolasDelage <nicolasdelage7@gmail.com>
2020-01-09 11:54:02 +0100

    feat: map notes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
 // === CONSTANTS
 
 const KEYBOARD_NOTES_MAPPING = {
-  q: { note: NOTES[0] },
+  q: { note: NOTES[0]},
+  w: { note: NOTES[1]},
+  e: { note: NOTES[2]},
+  r: { note: NOTES[3]},
+  t: { note: NOTES[4]},
+  y: { note: NOTES[5]},
+  u: { note: NOTES[6]},
+  i: { note: NOTES[7]},
+  o: { note: NOTES[8]},
+  p: { note: NOTES[9]},
+  a: { note: NOTES[10]},
+  s: { note: NOTES[11]},
+  // d: { note: NOTES[12]},
+  // f: { note: NOTES[13]},
+  // g: { note: NOTES[14]},
+  // h: { note: NOTES[15]},
+  // j: { note: NOTES[16]},
+  // k: { note: NOTES[17]},
+  // l: { note: NOTES[18]},
+  // z: { note: NOTES[19]},
+  // x: { note: NOTES[20]},
+  // c: { note: NOTES[21]},
+  // v: { note: NOTES[22]},
+  // b: { note: NOTES[23]},
 };
 
 // === SCENE ELEMENTS
@@ -57,6 +80,25 @@ const init = () => {
   updateViewportSize();
 
   document.body.appendChild(renderer.domElement);
+
+//  create notes group
+  let group = new THREE.Group();
+  let geometry = new THREE.BoxGeometry( 0.1, 0.6, 0.1 );
+  let material = new THREE.MeshBasicMaterial( {color: 0xffe4c4, vertexColors: THREE.FaceColors } );
+  let position = -1.5;
+
+  for(let i =0; i < 11; i++) {
+    let note = new THREE.Mesh( geometry, material );
+    note.geometry.faces[6].color = new THREE.Color(0xf0f8ff);
+    note.geometry.faces[7].color = new THREE.Color(0xf0f8ff);
+    note.position.set(position, 0.6, 0.1);
+    group.add(note);
+    position += 0.12;
+  }
+
+  group.rotation.set(-0.7, 0, 0)
+  scene.add(group)
+
 };
 
 const updateViewportSize = () => {
