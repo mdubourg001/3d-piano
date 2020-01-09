f7614f7574d16e686a22cfef39250ce35fcee07b
maxime.dubourg <maxime.dubourg@protonmail.com>
2020-01-09 15:08:28 +0100

    feat: color utils to get hex color code based from string

---
+++ src/color-utils.js
@@ -0,0 +1,26 @@
+const MAX_HEX_COLOR = 16777215;
+
+const arrayAverage = array =>
+  array.reduce((acc, cur) => acc + cur) / array.length;
+
+const decimalValueOfString = str => {
+  let decValue = '';
+  for (let i = 0; i < str.length; i++) {
+    decValue += str[i].charCodeAt(0);
+  }
+  return parseInt(decValue, 10);
+};
+
+const toHex = dec => dec.toString(16);
+
+const hexColorFromString = (strVal, maxVal, minVal = '\0') => {
+  const minDecimal = decimalValueOfString(minVal);
+  const decimalShiftedToZero = decimalValueOfString(strVal) - minDecimal;
+  const maxDecimalShiftedToZero = decimalValueOfString(maxVal) - minDecimal;
+
+  return toHex(
+    Math.round(
+      (decimalShiftedToZero * MAX_HEX_COLOR) / maxDecimalShiftedToZero,
+    ),
+  ).padEnd(6, '0');
+};
