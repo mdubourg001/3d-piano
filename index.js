6eede2cdb25fa89b018eeec32770b288496e1c1c
maxime.dubourg <maxime.dubourg@protonmail.com>
2020-01-09 09:38:24 +0100

    feat: setup of the three project + parcel

---
+++ index.js
@@ -0,0 +1,58 @@
+import * as THREE from "three";
+
+// === SCENE ELEMENTS
+
+let camera, scene, renderer;
+
+let resizeTimeout = null;
+
+const keyboardEvents = {};
+
+const init = () => {
+  camera = new THREE.PerspectiveCamera(
+    70,
+    window.innerWidth / window.innerHeight,
+    0.01,
+    10
+  );
+  camera.position.set(0, 0, 1);
+
+  scene = new THREE.Scene();
+
+  renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  // updating renderer size upon window.resize
+  window.addEventListener("resize", () => {
+    if (resizeTimeout !== null) clearTimeout(resizeTimeout);
+
+    resizeTimeout = setTimeout(() => {
+      renderer.setSize(window.innerWidth, window.innerHeight);
+    }, 100);
+  });
+
+  window.addEventListener("keydown", event => {
+    keyboardEvents[event.key] = true;
+  });
+
+  window.addEventListener("keyup", event => {
+    keyboardEvents[event.key] = false;
+  });
+
+  document.body.appendChild(renderer.domElement);
+};
+
+const handleKeyboardEvents = () => {};
+
+const gameLoop = () => {
+  requestAnimationFrame(gameLoop);
+
+  handleKeyboardEvents();
+};
+
+// ========== //
+
+init();
+renderer.render(scene, camera);
+
+gameLoop();
